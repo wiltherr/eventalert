cba39b5dab54625b67fe79a98e91588c2d74eeda
Tim Wittler <wittler.tim@gmail.com>
2017-12-28 17:44:29 +0100

    added documentation

--- eventalert/core/src/main/java/de/haw/eventalert/core/global/AlertEvents.java
+++ eventalert/core/src/main/java/de/haw/eventalert/core/global/AlertEvents.java
@@ -10,24 +10,60 @@ import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
 /**
- * Created by Tim on 19.08.2017.
+ * Util class for creating and converting AlertEvents.
+ * Is used by AlertEventConsumer- and -ProducerJobs.
+ *
+ * @see de.haw.eventalert.core.producer.example.ExampleProducerJob
+ * @see de.haw.eventalert.core.consumer.AlertEventConsumerJob
  */
 public class AlertEvents {
 
     private static final Logger LOG = LoggerFactory.getLogger(AlertEvents.class);
 
+    /**
+     * creates a new alertEvent of any POJO object
+     * <p>
+     * note: the original object will be converted to json.
+     * the {@link com.fasterxml.jackson.databind.ObjectMapper} will need the original object to implement
+     * a default constructor and getters/setters for all fields
+     *
+     * @param eventType   name of original (nested) event
+     * @param eventObject original event object (will be converted to json)
+     * @return a new AlertEvent
+     * @throws JsonProcessingException can be thrown if the passed eventObject is not valid for converting to json
+     */
     public static AlertEvent createEvent(String eventType, Object eventObject) throws JsonProcessingException {
         return new AlertEvent(eventType, eventObject);
     }
 
+    /**
+     * converts a AlertEvent to a json string
+     *
+     * @param alertEvent the alertEvent that should be converted
+     * @return the json representation of AlertEvent
+     * @throws JsonProcessingException can be thrown if alertEvent can not be converted
+     */
     public static String toJSONString(AlertEvent alertEvent) throws JsonProcessingException {
         return Utils.jsonMapper.writeValueAsString(alertEvent);
     }
 
+    /**
+     * converts a json alertEvent to {@link AlertEvent} object
+     *
+     * @param jsonAlertEvent json representation of alertEvent
+     * @return AlertEvent object
+     * @throws IOException can be thrown if the jsonAlertEvent string is not valid
+     */
     public static AlertEvent toAlertEvent(String jsonAlertEvent) throws IOException {
         return Utils.jsonMapper.readValue(jsonAlertEvent, AlertEvent.class);
     }
 
+    /**
+     * {@link FlatMapFunction} that converts a stream of {@link AlertEvent} to its json representation
+     *
+     * @return a stream of json representations as string
+     * @see #toJSONString(AlertEvent)
+     */
     public static FlatMapFunction<AlertEvent, String> convertToJSONString() {
         return (alertEvent, out) -> {
             try {
@@ -38,6 +74,12 @@ public class AlertEvents {
         };
     }
 
+    /**
+     * {@link FlatMapFunction} that converts a stream of alertEvent json strings to its object representation
+     *
+     * @return a stream of alertEvent objects
+     * @see #toAlertEvent(String)
+     */
     public static FlatMapFunction<String, AlertEvent> convertToAlertEvent() {
         return (jsonAlertEvent, out) -> {
             try {
--- eventalert/core/src/main/java/de/haw/eventalert/core/global/EventAlertConst.java
+++ eventalert/core/src/main/java/de/haw/eventalert/core/global/EventAlertConst.java
@@ -1,9 +1,15 @@
 package de.haw.eventalert.core.global;
 
 /**
- * Created by Tim on 04.09.2017.
+ * constants for EventAlert
  */
 public class EventAlertConst {
+    /**
+     * address / ip of Kafka broker
+     */
     public static final String KAFA_BROKER = "localhost:9092";
+    /**
+     * kafka topic which is used by {@link de.haw.eventalert.core.producer.AlertEventProducer} and {@link de.haw.eventalert.core.consumer.AlertEventConsumer}
+     */
     public static final String KAFKA_TOPIC_ALERTEVENT = "alert-events";
 }
