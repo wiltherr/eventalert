ece535e5aba0d8c2a3ed5b3ab5a7968cb5e59153
Tim <tim.wittler@haw-hamburg.de>
2018-01-02 00:39:07 +0100

    added app class

---
+++ eventalert/core/src/main/java/de/haw/eventalert/core/global/App.java
@@ -0,0 +1,17 @@
+package de.haw.eventalert.core.global;
+
+import org.apache.flink.client.program.ClusterClient;
+import org.apache.flink.client.program.StandaloneClusterClient;
+import org.apache.flink.configuration.Configuration;
+
+/**
+ * Created by Tim on 01.01.2018.
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        ClusterClient clusterClient = new StandaloneClusterClient(new Configuration());
+        //clusterClient.run(new JobWithJars())
+        //URL url = URI.create("target/")
+        //clusterClient.run(new JobWithJars(null, new URL("target/core.jar")))
+    }
+}
